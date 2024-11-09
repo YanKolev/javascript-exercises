815fbe0af597167bc40e2774855e8dca97e5a30f
Yan Kolev <y.kolev11@gmail.com>
2024-11-09 15:29:47 +0100

    exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  //since we need to repeat a string we will need to start with a variable that
+  //holds empty sting
 
+  let repString = ""; // will store the final repeated string
+
+  if (num < 0) {
+    return "ERROR";
+  }
+
+  for (let i = 0; i < num; i++) {
+    repString += string;
+  }
+
+  return repString;
 };
 
 // Do not edit below this line
