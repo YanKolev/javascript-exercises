0ef1aaf0ee35c8fb964e30f071a6242beb4a3b73
Yan Kolev <y.kolev11@gmail.com>
2024-11-28 11:55:32 +0100

    solution 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+// const removeFromArray = function (arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === 3) {
+//       arr.splice(i, 1);
+//       i--;
+//     }
+//   }
+
+//   return arr;
+// };
+
+const removeFromArray = function (arr, ...args) {
+  let arr2 = arr.filter((a) => !args.includes(a));
+
+  return arr2;
 };
 
 // Do not edit below this line
