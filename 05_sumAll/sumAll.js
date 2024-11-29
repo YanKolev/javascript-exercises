274a91f64ced533c8ded050bf1f2124e76ac2181
Yan Kolev <y.kolev11@gmail.com>
2024-11-29 18:41:05 +0100

    exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  let sum = 0;
+  for (let i = a; i <= b; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+//positive integers, throw error if its negative,
+//variable to hold the final sum
+//loop through the given numbers
+//for each iteration add the num + sum
+//return sum after finishing the loop
+// 2 passed, 5 failed ... .needs rework
